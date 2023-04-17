26112198c1524e5186a10797b1bd5c8266972793
Ajay <115636212+ajaygithub2@users.noreply.github.com>
2023-04-17 18:29:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import datetime
+from dateutil import relativedelta
+
+DOB = st.date_input("Enter your DOB", min_value=datetime.datetime(1950,1,1))
+
+today = datetime.date.today()
+
+delta = relativedelta.relativedelta(today, DOB)
+
+st.write(f"You have been on this earth for {delta.years} years {delta.months} months and {delta.days} days.")
